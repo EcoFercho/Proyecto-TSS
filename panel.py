901830124bc7426846513b0602813e6a230186f2
EcoFercho <erwin0pisis@gmail.com>
2025-06-26 10:46:40 -0400

    Deploy de produccion implementado ultimas  funcionalidades

--- panel.py
+++ panel.py
@@ -78,16 +78,13 @@ interfaz.register_callbacks(app)
 generarReporte.register_callbacks(app)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
     
 
-
-
-# Para deploy 
-# Exponer servidor para Gunicorn en producción
-# server = app.server
-# if __name__ == '__main__':
-#     import os
-#     port = int(os.environ.get('PORT', 8050))
-#     app.run(host='0.0.0.0', port=port, debug=True)
+# Para deploy - Exponer servidor para Gunicorn en producción
+server = app.server
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 8050))
+    app.run(host='0.0.0.0', port=port, debug=True)
